070cfe5001f66c4ceb5f0236a35045564b18aae7
jeongmin <sssi6792@gmail.com>
2023-04-12 01:18:38 +0000

    [ADD]EarlyStopping

--- code/arguments.py
+++ code/arguments.py
@@ -3,9 +3,9 @@ import argparse
 def get_args():
     # 기본 설정값만 남기고, hyperparameter는 wandb에게 맡김.
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', default='klue/roberta-small', type=str)
-    parser.add_argument('--batch_size', default=16, type=int)
-    parser.add_argument('--max_epoch', default=3, type=int)
+    parser.add_argument('--model_name', default='klue/roberta-large', type=str)
+    parser.add_argument('--batch_size', default=8, type=int)
+    parser.add_argument('--max_epoch', default=15, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
     parser.add_argument('--train_path', default='./data/train.csv')
